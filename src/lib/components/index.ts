1b9bd3febf7bb9624f664d2caa0fcf206e186cf1
Alez05 <cercel.alexandru436@gmail.com>
2026-06-22 22:17:37 +0200

    feat: password length and checkbox states implemented - looking to simplify the code from cehckbox component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -6,3 +6,8 @@ export { CheckBoxInput } from "./checkbox_input";
 export { RangeInput } from "./range_input";
 export { StrengthMeter } from "./strength_meter";
 export { TextBox } from "./textbox_input";
+
+
+//component types
+
+export type { TRange } from './type'
--- src/lib/components/range_input.tsx
+++ src/lib/components/range_input.tsx
@@ -1,17 +1,26 @@
-const RangeInput = () => {
+"use client"
+import { ChangeEvent, useState } from "react";
+import "../styles/range_input.css";
+import { TRange } from "./type";
+
+const RangeInput = ({passwordLength, setPasswordLength}:TRange) => {
+
   return (
-    <div>
-      <form action="">
-        <label htmlFor="frange">Range 1-10</label><br />
-        <input
-          type="range"
-          id="frange"
-          min={0}
-          max={15}
-          name="frange"
-          className="frange"
-        />
-      </form>
+    <div className="rcontainer">
+      <h2 className="rtitle">Character length</h2>
+      <input
+        type="range"
+        id="frange"
+        min={8}
+        max={16}
+        value={passwordLength}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setPasswordLength(Number(e.target.value))}
+        name="frange"
+        className="slider"
+      />
+      <p>
+        Value: {passwordLength}
+      </p>
     </div>
   );
 };
